34c5a79d9a25d52ac618a5878d45a93736372f28
G24 <quanhung309.ht@gmail.com>
2023-10-16 20:33:41 +0700

    [ui] - create loading page 2

--- app/src/main/java/com/example/airplane_android/InitialPageActivity.java
+++ app/src/main/java/com/example/airplane_android/InitialPageActivity.java
@@ -1,6 +1,5 @@
 package com.example.airplane_android;
 
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -11,47 +10,27 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class InitialPage extends AppCompatActivity {
-
-    private ImageView imgLogo;
-    private TextView textLogo;
+public class InitialPageActivity extends AppCompatActivity {
     private Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_initial_page);
-
-        imgLogo = (ImageView) findViewById(R.id.imgLogo);
-        textLogo = (TextView) findViewById(R.id.textLogo);
-
-        imgLogo.setVisibility(View.INVISIBLE);
-        textLogo.setVisibility(View.INVISIBLE);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
 
-        imgLogo = (ImageView) findViewById(R.id.imgLogo);
-        textLogo = (TextView) findViewById(R.id.textLogo);
-
         handler = new Handler(Looper.getMainLooper());
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                imgLogo.setVisibility(View.VISIBLE);
-                textLogo.setVisibility(View.VISIBLE);
-            }
-        }, 2000);
-
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(InitialPage.this, MainActivity.class);
+                Intent intent = new Intent(InitialPageActivity.this, MainActivity.class);
                 startActivity(intent);
             }
-        }, 5000);
+        }, 3000);
 
     }
 }
