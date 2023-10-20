961d31ff0e8dd2cd448af098c254b5b16e4056f8
begasic2001 <luongminhthanh91@gmail.com>
2023-10-21 00:01:33 +0700

    Create New Plane Success!!!!!!!!

---
+++ app/src/main/java/com/example/airplane_android/admin/PlaneAddActivity.java
@@ -0,0 +1,16 @@
+package com.example.airplane_android.admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.airplane_android.R;
+
+public class PlaneAddActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_plane_add);
+    }
+}
