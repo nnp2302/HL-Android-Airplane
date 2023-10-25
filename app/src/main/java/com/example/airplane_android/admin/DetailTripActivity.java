5530f830c29ff7f115eec0cc74f2022f19b9cbfc
begasic2001 <luongminhthanh91@gmail.com>
2023-10-25 13:19:54 +0700

    Lums Tiens Mr. Hưng

--- app/src/main/java/com/example/airplane_android/admin/DetailTripActivity.java
+++ app/src/main/java/com/example/airplane_android/admin/DetailTripActivity.java
@@ -9,14 +9,32 @@ import com.example.airplane_android.R;
 import com.example.airplane_android.admin.model.Trip;
 
 public class DetailTripActivity extends AppCompatActivity {
-    TextView textIdTripDetails;
+    TextView textFrom,textTo,textStart,textEnd,textEconomyPrice,textBusinessPrice,textEconomyTicket,textBusinessTicket,textSelectedPlane;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_trip);
-        textIdTripDetails = findViewById(R.id.textIdTripDetail);
+        textFrom = findViewById(R.id.textTripDetailFrom);
+        textTo = findViewById(R.id.textTripDetailTo);
+        textStart = findViewById(R.id.textTripDetailStart);
+        textEnd = findViewById(R.id.textTripDetailEnd);
+        textEconomyPrice = findViewById(R.id.textTripDetailEconomyPrice);
+        textBusinessPrice = findViewById(R.id.textTripDetailBusinessPrice);
+        textEconomyTicket = findViewById(R.id.textTripDetailEconomyTicket);
+        textBusinessTicket = findViewById(R.id.textTripDetailBusinessTicket);
+        textSelectedPlane = findViewById(R.id.textTripDetailPlaneSelect);
+
+
         Bundle bundle = getIntent().getExtras();
         Trip trip = (Trip) bundle.get("trip");
-        textIdTripDetails.setText(trip.getId());
+        textFrom.setText(trip.getFrom());
+        textTo.setText(trip.getTo());
+        textStart.setText(trip.getStart());
+        textEnd.setText(trip.getEnd());
+        textEconomyPrice.setText(trip.getEconomyPrice().toString());
+        textBusinessPrice.setText(trip.getBusinessPrice().toString());
+        textEconomyTicket.setText(trip.getEconomyTicket().toString());
+        textBusinessTicket.setText(trip.getBusinessTicket().toString());
+        textSelectedPlane.setText(trip.getPlaneId());
     }
 }
